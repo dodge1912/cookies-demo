a9789e82bcb86a61d75802f2102581cf95cafc81
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-05-07 15:34:08 +0200

    Write /getCookie route on express.js server

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-// app.use(express.static('public'));
-
 app.use(cookieParser());
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/getCookie', function(req, res) {
+  res.cookie('hello', 'world', {maxAge: 900000, httpOnly: false});
+  return res.status(200).send('Cookie sent');
+});
 
 app.listen(8080);
